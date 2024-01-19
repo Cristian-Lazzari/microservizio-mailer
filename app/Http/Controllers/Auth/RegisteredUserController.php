812ee89d7eda3ef4bf1fe3545f8ef221ebb1a6d3
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2024-01-19 13:04:47 +0100

    dfs

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -13,31 +13,6 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
 
-// class RegisteredUserController extends Controller
-// {
-//     /**
-//      * Display the registration view.
-//      */
-//     public function create(): View
-//     {
-//         // Se la registrazione è disabilitata, reindirizza altrove
-//         return redirect('/');
-//     }
-
-//     /**
-//      * Handle an incoming registration request.
-//      *
-//      * @throws \Illuminate\Validation\ValidationException
-//      */
-//     public function store(Request $request): RedirectResponse
-//     {
-//         // Se la registrazione è disabilitata, reindirizza altrove
-//         return redirect('/');
-//     }
-// }
-
-
-
 class RegisteredUserController extends Controller
 {
     /**
@@ -45,7 +20,8 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        // Se la registrazione è disabilitata, reindirizza altrove
+        return redirect('/');
     }
 
     /**
@@ -55,22 +31,46 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
-
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        event(new Registered($user));
-
-        Auth::login($user);
-
-        return redirect(RouteServiceProvider::HOME);
+        // Se la registrazione è disabilitata, reindirizza altrove
+        return redirect('/');
     }
 }
+
+
+
+// class RegisteredUserController extends Controller
+// {
+//     /**
+//      * Display the registration view.
+//      */
+//     public function create(): View
+//     {
+//         return view('auth.register');
+//     }
+
+//     /**
+//      * Handle an incoming registration request.
+//      *
+//      * @throws \Illuminate\Validation\ValidationException
+//      */
+//     public function store(Request $request): RedirectResponse
+//     {
+//         $request->validate([
+//             'name' => ['required', 'string', 'max:255'],
+//             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+//             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+//         ]);
+
+//         $user = User::create([
+//             'name' => $request->name,
+//             'email' => $request->email,
+//             'password' => Hash::make($request->password),
+//         ]);
+
+//         event(new Registered($user));
+
+//         Auth::login($user);
+
+//         return redirect(RouteServiceProvider::HOME);
+//     }
+// }
